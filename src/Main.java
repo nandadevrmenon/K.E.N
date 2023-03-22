4b56cf7cc368d7e50aab30a4fb04bf757f2f1ff9
Enzo Crema <enzo.crema@student.griffith.ie>
2023-03-22 10:08:41 +0000

    Integrating of OpenWeatherAPI

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,33 @@
-package src;
+import com.github.prominence.openweathermap.api.OpenWeatherMapClient;
+
+import java.io.File;
 
 public class Main {
+    private static String API_TOKEN;
+
     public static void main(String[] args) {
+
+        try {
+            File API_TOKEN_FILE = new File("resources/API_KEY.txt");
+            API_TOKEN = new java.util.Scanner(API_TOKEN_FILE).useDelimiter("\\Z").next();
+        } catch (Exception e) {
+            System.out.println("API_KEY.txt not found");
+        }
+
         System.out.println("Hello, World!");
+
+        OpenWeatherMapClient openWeatherClient = new OpenWeatherMapClient(API_TOKEN);
+
+        /*
+        final Weather weather = openWeatherClient.currentWeather()
+                .single()
+                .byCityName("London")
+                .unitSystem(UnitSystem.METRIC)
+                .retrieve().asJava();
+
+
+        System.out.println(weather.getTemperature());
+
+         */
     }
 }
