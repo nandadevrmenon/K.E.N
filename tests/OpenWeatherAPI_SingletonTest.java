1152abe9fb563f783a1645b2a0b5709295234be3
Kacper Wojnarowski <kacpermaciej.wojnarowski@student.griffith.ie>
2023-03-22 11:57:50 +0000

    test connection to weather api

--- tests/OpenWeatherAPI_SingletonTest.java
+++ tests/OpenWeatherAPI_SingletonTest.java
@@ -1,5 +1,21 @@
+import com.github.prominence.openweathermap.api.OpenWeatherMapClient;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class OpenWeatherAPI_SingletonTest {
+
+        @Test
+        void getInstance() {
+            OpenWeatherAPI_Singleton openWeatherAPI_singleton = new OpenWeatherAPI_Singleton();
+            OpenWeatherMapClient openWeatherClient = OpenWeatherAPI_Singleton.getInstance();
+
+            if(openWeatherClient == null) {
+                System.out.println("OpenWeatherClient is null");
+            }
+            else {
+                System.out.println("Connected to OpenWeatherClient");
+            }
+        }
 
 }
