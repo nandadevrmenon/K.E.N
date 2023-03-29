7972fc2f2ec387192d83a9d6b61e7b7cc288fc8b
Enzo Crema <enzo.crema@student.griffith.ie>
2023-03-29 11:06:17 +0100

    Implementation OpenAI API using HTTP Requests

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,27 @@
-import com.github.prominence.openweathermap.api.OpenWeatherMapClient;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import json.JSONObject;
+import netscape.javascript.JSObject;
+import org.apache.hc.client5.http.classic.methods.HttpPost;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
 
-import java.io.File;
 
 public class Main {
-    private static String API_TOKEN;
-
+    private static String OPENAI_API_TOKEN = "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";
     public static void main(String[] args) {
 
-    EasyWeather weather = new EasyWeather();
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost("https://api.openai.com/v1/engines/davinci-codex/completions");
+        httpPost.setHeader("Authorization", "Bearer " + OPENAI_API_TOKEN);
+        httpPost.setHeader("Content-Type", "application/json");
+
+        JSONPObject requestBody = new JSONPObject();
+        requestBody.put("prompt", "Hello, World!");
+        requestBody.put("max_tokens", 5);
+
+
+
+        EasyWeather weather = new EasyWeather();
         System.out.println(weather.getWeatherByCity("mumbai"));
     }
 }
