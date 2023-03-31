f4f0382a367470b3dddf3c406c18f54247392db5
nandadev <nandadev.rajeevmenon@student.griffith.ie>
2023-03-31 12:36:23 +0100

    Add functionality to getThreeDay'X'Starting() methods

--- src/EasyWeather.java
+++ src/EasyWeather.java
@@ -10,6 +10,7 @@ import com.github.prominence.openweathermap.api.model.weather.Weather;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class EasyWeather {
     private OpenWeatherMapClient weatherClient = OpenWeatherAPI_Singleton.getInstance();
@@ -108,37 +109,104 @@ public class EasyWeather {
         return rain.getOneHourLevel();
     }
 
-    public ArrayList<Double> getThreeDayTempStarting(LocalDate startDate, ArrayList<String> Locations){
+    public ArrayList<Double> getThreeDayTempStarting(LocalDate startDate, String city){
+        LocalDate now = LocalDate.now();
+        System.out.println(startDate.isAfter(now));
+        System.out.println(startDate.isBefore(now.plusDays(2)));
+        if(startDate.isAfter(now.plusDays(2)) || (startDate.isBefore(now))){
+            throw new IllegalArgumentException("The trip has to end before 5 days from now.");
+        }
         ArrayList<Double> temperatures = new ArrayList<Double>();
+        ArrayList<WeatherForecast> threeDayForecast= getThreeDayForecast(city,startDate);
+        for(int i = 0;i<threeDayForecast.size();i++){
+            temperatures.add(threeDayForecast.get(i).getTemperature().getValue());
+        }
+
+
         return temperatures;
     }
 
-    public ArrayList<Double> getThreeDayHumidityStarting(LocalDate startDate, ArrayList<String> Locations){
+    public ArrayList<Double> getThreeDayHumidityStarting(LocalDate startDate, String city){
+        LocalDate now = LocalDate.now();
+        System.out.println(startDate.isAfter(now));
+        System.out.println(startDate.isBefore(now.plusDays(2)));
+        if(startDate.isAfter(now.plusDays(2)) || (startDate.isBefore(now))){
+            throw new IllegalArgumentException("The trip has to end before 5 days from now.");
+        }
         ArrayList<Double> humidities = new ArrayList<Double>();
+        ArrayList<WeatherForecast> threeDayForecast= getThreeDayForecast(city,startDate);
+        for(int i = 0;i<threeDayForecast.size();i++){
+            humidities.add((double)threeDayForecast.get(i).getHumidity().getValue());
+        }
         return humidities;
     }
 
-    public ArrayList<Double> getThreeDayWindStarting(LocalDate startDate, ArrayList<String> Locations){
+    public ArrayList<Double> getThreeDayWindStarting(LocalDate startDate, String city){
+        LocalDate now = LocalDate.now();
+        System.out.println(startDate.isAfter(now));
+        System.out.println(startDate.isBefore(now.plusDays(2)));
+        if(startDate.isAfter(now.plusDays(2)) || (startDate.isBefore(now))){
+            throw new IllegalArgumentException("The trip has to end before 5 days from now.");
+        }
         ArrayList<Double> winds = new ArrayList<Double>();
+        ArrayList<WeatherForecast> threeDayForecast= getThreeDayForecast(city,startDate);
+        for(int i = 0;i<threeDayForecast.size();i++){
+            winds.add((double)threeDayForecast.get(i).getWind().getSpeed());
+        }
         return winds;
     }
 
-    public ArrayList<Double> getThreeDayCloudStarting(LocalDate startDate, ArrayList<String> Locations){
+    public ArrayList<Double> getThreeDayCloudStarting(LocalDate startDate, String city){
+        LocalDate now = LocalDate.now();
+        System.out.println(startDate.isAfter(now));
+        System.out.println(startDate.isBefore(now.plusDays(2)));
+        if(startDate.isAfter(now.plusDays(2)) || (startDate.isBefore(now))){
+            throw new IllegalArgumentException("The trip has to end before 5 days from now.");
+        }
         ArrayList<Double> cloudcovers = new ArrayList<Double>();
+        ArrayList<WeatherForecast> threeDayForecast= getThreeDayForecast(city,startDate);
+        for(int i = 0;i<threeDayForecast.size();i++){
+            cloudcovers.add((double)threeDayForecast.get(i).getClouds().getValue());
+        }
         return cloudcovers;
     }
 
 
-    private ArrayList<WeatherForecast> getFiveDayForecast(){
-        Forecast forecast = weatherClient
-                .forecast5Day3HourStep()
-                .byCityName("tallaght")
-                .language(Language.ENGLISH)
-                .unitSystem(UnitSystem.METRIC)
-                .count(40)
-                .retrieve()
-                .asJava();
-        return new ArrayList<WeatherForecast>(forecast.getWeatherForecasts());
+    public ArrayList<WeatherForecast> getThreeDayForecast(String city,LocalDate startDate){
+        Forecast fiveDaysWeather;
+        try{
+            fiveDaysWeather = weatherClient
+                    .forecast5Day3HourStep()
+                    .byCityName(city)
+                    .language(Language.ENGLISH)
+                    .unitSystem(UnitSystem.METRIC)
+                    .count(40)
+                    .retrieve()
+                    .asJava();      //returns 5 day forecast with 3 hour diff in each timestamp
+        }catch(NoDataFoundException ndfe){
+            if(ndfe.getMessage().contains("Data for provided parameters wasn't found")){
+                throw new IllegalArgumentException("The location entered might not be a valid location.");
+            }
+            else return null;
+        }
+
+        //we extract 3day forecast for only 9am and 6pm of each day
+        ArrayList<WeatherForecast> fiveDayForecast = new ArrayList<WeatherForecast>(fiveDaysWeather.getWeatherForecasts());
+        Iterator<WeatherForecast> fdit = fiveDayForecast.iterator();
+        ArrayList<WeatherForecast> threeDayForecast = new ArrayList<WeatherForecast>();
+
+        int count =0;
+        while(fdit.hasNext() && count<6){
+            WeatherForecast forecast = fdit.next();
+            if(forecast.getForecastTime().isBefore(startDate.atStartOfDay())){continue;}
+            if(forecast.getForecastTime().getHour()==19 || forecast.getForecastTime().getHour()==10){
+                System.out.println(forecast);
+                threeDayForecast.add(forecast);
+                count++;
+            }
+
+        }
+        return threeDayForecast;
     }
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
+import com.github.prominence.openweathermap.api.model.forecast.WeatherForecast;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Main {
     private static String API_TOKEN;
@@ -5,9 +10,23 @@ public class Main {
     public static void main(String[] args) {
 
     EasyWeather weather = new EasyWeather();
-        System.out.println(weather.getTemperatureByCity("bihar,india"));
-        System.out.println(weather.getWeatherByCity("dublin,ire"));
 
-        System.out.println(weather.getRainByCity("waco,usa"));
+
+        ArrayList<String> locations = new ArrayList<String>();
+        locations.add("cork,ireland");
+        locations.add("dublin,ireland");
+        locations.add("galway,ireland");
+        locations.add("wicklow,ireland");
+        locations.add("tallaght,ireland");
+        ArrayList<Double> temps = weather.getThreeDayTempStarting(LocalDate.now(),"helsinki");
+        ArrayList<Double> humidities = weather.getThreeDayHumidityStarting(LocalDate.now(),"helsinki");
+        ArrayList<Double> cloudcovers = weather.getThreeDayCloudStarting(LocalDate.now(),"helsinki");
+        ArrayList<Double> winds = weather.getThreeDayWindStarting(LocalDate.now(),"helsinki");
+
+        System.out.println(temps);
+        System.out.println(humidities);
+        System.out.println(cloudcovers);
+        System.out.println(winds);
+
     }
 }
