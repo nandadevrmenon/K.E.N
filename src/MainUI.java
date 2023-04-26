2189355d459471712ea767d9840f8bc0fbb0bd54
Enzo Crema <zephirfx.pro@gmail.com>
2023-04-27 00:43:15 +0100

    Modifying UI, now working with Listview and different string buffers for human and bot

--- src/MainUI.java
+++ src/MainUI.java
@@ -1,22 +1,28 @@
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.Objects;
-import java.util.function.Function;
+import java.util.ResourceBundle;
 
-public class MainUI extends Application {
+public class MainUI extends Application implements Initializable {
 
     public AnchorPane welcomeWindow;
     public TextField messageBar;
-    public TextArea answerArea;
+    public ObservableList<String> data = FXCollections.observableArrayList();
+    public ListView<String> listView = new ListView<>();
 
     public static void main(String[] args) {
         launch();
@@ -25,50 +31,43 @@ public class MainUI extends Application {
     @Override
     public void start(Stage stage) {
         Parent root;
+
         try { // load the UI from the FXML file
             root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("UI.fxml")));
         } catch (IOException e) { // if the FXML file is not found or null, throw an exception
             throw new RuntimeException(e);
+
         }
 
+        // load icon
+        stage.getIcons().add(new Image("image.png"));
         // load style
         root.getStylesheets().add("style.css");
-
         // set the title of the window
         stage.setTitle("Weather App");
-
         // set the scene of the window
         stage.setScene(new javafx.scene.Scene(root));
-
-        // hide the window when button click
-        stage.setOnCloseRequest(event -> {
-            stage.hide();
-            event.consume();
-        });
-
-
-
         // show the window
         stage.show();
-
     }
-
-
 
     @FXML
-    private void printHelloWorld(ActionEvent event) {
+    private void onClick(ActionEvent event) {
         event.consume();
         // hide welcomeWindow when button click
-        welcomeWindow.setVisible(false);
+        if (welcomeWindow.isVisible() && !messageBar.getText().isEmpty()) {
+            welcomeWindow.setVisible(false);
+        }
 
-        // show messageBar and answerArea when button click
-        String message = messageBar.getText();
-
-        // clear messageBar when button click
-        messageBar.clear();
-
-        // print message in answerArea when button click
-        answerArea.appendText(message + "\n");
+        // get the message from the messageBar
+        if (!messageBar.getText().isEmpty()) {
+            data.add("\uD83D\uDC66 : " + messageBar.getText());
+            messageBar.clear();
+        }
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        listView.setItems(data);
+    }
 }
